f0e93d2fd8a6855a3c141300802c3f60fe6c0079
krazy-99 <ebrammessi.lge@gmail.com>
2026-09-18 23:49:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -59,21 +59,21 @@ class KrazyLinkModal(discord.ui.Modal, title="Paste Your Roblox Link"):
         required=True
     )
 
-    # TRACE REDIRECT
-api_url = f"https://api.redirect-checker.net/?url={original_url}&timeout=5"
-try:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(api_url) as resp:
-            data = await resp.json()
+        # TRACE REDIRECT
+    api_url = f"https://api.redirect-checker.net/?url={original_url}&timeout=5"
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api_url) as resp:
+                data = await resp.json()
 
-            redirects = data.get("redirects", [])
-            if redirects:
-                final_url = redirects[-1].get("url", original_url)
-            else:
-                final_url = original_url
+                redirects = data.get("redirects", [])
+                if redirects:
+                    final_url = redirects[-1].get("url", original_url)
+                else:
+                    final_url = original_url
 
-except Exception:
-    final_url = original_url
+    except Exception:
+        final_url = original_url
 
 
         # Extract user ID
